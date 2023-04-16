3d57b58d7b3c38b68cf61437de6895d1d3d9853d
Rajan-007 <aravinthrajan034@gmail.com>
2023-04-16 07:52:07 +0530

    Conditional Rendering

--- myapp/src/components/ListGroup.js
+++ myapp/src/components/ListGroup.js
@@ -1,16 +1,21 @@
 
 
 function ListGroup(){
-    const items=['HI',
-    'Hello',
-    'Hey',
-    'Nice'];
-    return(
-        <ul class='List-group'>
+    let items=['HI','Hello','Hey','Nice'];
 
-            {items.map(item => <li> {item} </li>)}
-            
-        </ul>
+     items=[];
+
+    // if( items.length === 0)
+    //     return<p>no item in the list</p>
+    return (
+        <>
+            <h1> List </h1>
+            { items.length === 0 ? <p> No item in the list</p> :null }
+            <ul class='List-group'>
+                {items.map((item) => (<li> {item} </li>))}
+
+            </ul>
+        </>
     );
 };
 export default ListGroup;
