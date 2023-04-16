a1be2daaee33714c5ebfa1a00554957ad6c47460
Rajan-007 <aravinthrajan034@gmail.com>
2023-04-16 16:33:30 +0530

    Toastify added

--- myapp/src/components/ListGroup.js
+++ myapp/src/components/ListGroup.js
@@ -1,21 +1,43 @@
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 
 function ListGroup(){
     let items=['HI','Hello','Hey','Nice'];
-
-     items=[];
-
+    
+    //  items=[];
+     const notify = () => toast.success("Wow , List is so easy!");
     // if( items.length === 0)
     //     return<p>no item in the list</p>
     return (
         <>
             <h1> List </h1>
             { items.length === 0 ? <p> No item in the list</p> :null }
-            <ul class='List-group'>
-                {items.map((item) => (<li> {item} </li>))}
-
+            <ul className='List-group'>
+                {items.map((item) => (<li key={item} > {item} </li>))}
             </ul>
+                <div>
+                    <button onClick={notify}>Notify!</button>
+                    <ToastContainer />
+                </div>
         </>
     );
 };
 export default ListGroup;
+
+// import React from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// function ListGroup(){
+//   const notify = () => toast("Wow so easy!");
+
+//   return (
+//     <div>
+//       <button onClick={notify}>Notify!</button>
+//       <ToastContainer />
+//     </div>
+//   );
+// }
+
+// export default ListGroup;
